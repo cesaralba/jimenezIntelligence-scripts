61fdc9fb19a92073af0bab2ba15149b0b56c457a
cap@CASA <cesar.alba@gmail.com>
2018-01-05 15:19:23 +0100

    getSuperManager.sh: + Baja el codigo de github + Parametrizado + Comprime los ficheros con fecha + Usuario y contraseña llevados a sysconfig

--- getSuperManager.sh
+++ getSuperManager.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
+CONFIGFILE=/etc/sysconfig/SuperManager
+
+REPOURL="git@github.com:cesaralba/jimenezIntelligence.git"
+BASEDIR=$(cd "$(dirname $(readlink -e $0))/../" && pwd )
+WRKDIR="${BASEDIR}/wrk"
 TODAY=$(date '+%Y%m%d%H%M')
+
+[ -d ${WRKDIR} ] && rm -rf  ${WRKDIR}
+mkdir -p ${WRKDIR}
+git clone -q --branch master ${REPOURL} ${WRKDIR}
+
+if [ $? != 0 ]
+then
+  echo "$0: Problems with GIT. Bye"
+  exit 1
+fi
 
 source /home/calba/devel/SuperManagerPython/SACBenv/bin/activate
 
-ORIGSMFILE="/home/calba/devel/SuperManager/datos/SM2017.latest.p"
-DESTSMFILE="/home/calba/devel/SuperManager/datos/SM2017.newest.p"
-DESTSMFILEDATED="/home/calba/devel/SuperManager/datos/SM2017.${TODAY}.p"
+[ -f ${CONFIGFILE} ] && source ${CONFIGFILE}
 
-python /home/calba/Dropbox/devel/SuperManagerPython/BrowseSuperManager.py -u mirza15 -p delibasi -i ${ORIGSMFILE} -o ${DESTSMFILE}
+ORIGSMFILE="${BASEDIR}/datos/SM2017.latest.p"
+DESTSMFILE="${BASEDIR}/datos/SM2017.newest.p"
+DESTSMFILEDATED="${BASEDIR}/datos/SM2017.${TODAY}.p"
+
+python ${WRKDIR}/BrowseSuperManager.py -u ${SM_USER} -p ${SM_PASSWORD} -i ${ORIGSMFILE} -o ${DESTSMFILE}
 
 if [ $? = 0 ]
 then
   if [ -f ${DESTSMFILE} ]
   then
     cp ${DESTSMFILE} ${DESTSMFILEDATED}
+    gzip -9 ${DESTSMFILEDATED}
     mv ${DESTSMFILE} ${ORIGSMFILE}
   fi
 fi  
