06400f21dbd392eabd5f67f09a7503a260e8aaf3
cesaralba <7543406+cesaralba@users.noreply.github.com>
2018-10-20 12:40:11 +0200

    Eliminado copy2Dropbox
    Es una cosa local mía.

--- runAll.sh
+++ runAll.sh
@@ -7,5 +7,3 @@ bash ${BASEDIR}/bin/buildDataTree.sh
 bash ${BASEDIR}/bin/getMercado.sh
 bash ${BASEDIR}/bin/getTemporada.sh
 bash ${BASEDIR}/bin/getSuperManagerMerged.sh -y
-bash ${BASEDIR}/bin/copy2Dropbox.sh
-
